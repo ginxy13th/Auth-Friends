5f84bde7fdfb53792154a71a5cd11fb34e5b06ff
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-08-19 11:47:23 -0400

    grr

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/friends/friendlist';
+import Login from './components/LoginPage';
+import AddFriend from './components/friends/AddFriends';
+import axiosWithAuth from './axios/axios';
+import { FriendsContext } from './components/context';
 
 function App() {
+  const [friends, setFriends] = useState([]);
+
+  const getFriends = () => {
+    axiosWithAuth()
+      .get('/api/friends')
+      .then((res) => {
+        setFriends(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []);
+
+  const postNewFriend = (friend) => {
+    axiosWithAuth()
+      .post('/api/friends', friend)
+      .then((res) => {
+        setFriends([]);
+        getFriends();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deleteFriend = (id) => {
+    axiosWithAuth()
+      .delete(`/api/friends/${id}`)
+      .then((res) => {
+        setFriends([]);
+        getFriends();
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <FriendsContext.Provider
+        value={{
+          setFriends,
+          getFriends,
+          friends,
+          postNewFriend,
+          deleteFriend,
+        }}
+      >
+        <div>
+          <header>
+            <h3>FriendsList</h3>
+            <Link to="/">
+              Home
+            </Link>
+            <Link to="/Login">
+              Login
+            </Link>
+            <Link to="/Friends">
+              Friends
+            </Link>
+          </header>
+        </div>
+        <div>
+          <Switch>
+            <PrivateRoute path="/friends" component={FriendsList} />
+            <PrivateRoute path="/add-friend" component={AddFriend} />
+            <Route path="/login" component={Login} />
+            <Route exact path="/">
+              <h1>Welcome to FriendsList</h1>
+            </Route>
+          </Switch>
+        </div>
+      </FriendsContext.Provider>
+    </BrowserRouter>
   );
 }
 
